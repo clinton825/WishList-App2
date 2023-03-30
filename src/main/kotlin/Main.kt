04539ba15a15b3566528ff1c0ff3a3a69af4b16a
20097793 <20097793@mail.wit.ie>
2023-03-30 11:03:17 +0100

    Writing skeleton menu for WishList App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,63 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.System.exit
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val scanner = Scanner(System.`in`)
+
+fun main(args: Array<String>) {
+    runMenu()
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1 -> addWishList()
+            2 -> listWishList()
+            3 -> updateWishList()
+            4 -> deleteWishList()
+            0 -> exitApp()
+            else -> println("Invalid option entered: ${option}")
+
+        }
+    } while (true)
+}
+
+
+fun mainMenu() : Int {
+    print(""" 
+         > ----------------------------------
+         > |        SHOPPING WISHLIST APP   |
+         > ----------------------------------
+         > |  WISHLIST MENU                 |
+         > |   1) Add a WishList            |
+         > |   2) List all WishLists        |
+         > |   3) Update the WishList       |
+         > |   4) Delete a WishList         |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
+    return scanner.nextInt()
+}
+
+
+fun addWishList() {
+    println("You chose Add WishList")
+}
+
+fun listWishList() {
+    println("You chose List WishLists")
+}
+
+fun updateWishList() {
+    println("You chose Update WishList")
+}
+
+fun deleteWishList() {
+    println("You chose Delete WishList")
+}
+
+fun exitApp() {
+    println("Exiting...bye")
+    exit(0)
 }
